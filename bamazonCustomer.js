8245a2a3290770bfb1a7275ed3e1b64ba97f4d90
T3k5upp0rt <kylan.childers@gmail.com>
2019-09-14 20:07:31 -0700

    Added javascript logic for bamazon customer facing interface for purchasing items

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,5 +18,65 @@ var connection = mysql.createConnection({
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    runPrompt();
+    listProducts();
   });
+
+function listProducts() {
+  connection.query("SELECT item_id, product_name, department_name, price, stock_quantity FROM products", function(err, results) {
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+          name: "item",
+          type: "list",
+          choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].product_name);
+            }
+            return choiceArray;
+          },
+          message: "What product would you like to buy?"
+        },
+        {
+          name: "buy",
+          type: "input",
+          message: "How many of units of this product would you like to buy?"
+        }
+      ])
+      .then(function(answer) {
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].product_name === answer.item) {
+            chosenItem = results[i];
+            console.table(chosenItem)
+          }
+        }
+        if (chosenItem.stock_quantity >= parseInt(answer.buy)) {
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+              {
+                stock_quantity: (chosenItem.stock_quantity - answer.buy),
+                product_sales: (chosenItem.product_sales + (chosenItem.price * answer.buy)),
+                units_sold: (chosenItem.units_sold + parseInt(answer.buy))
+              },
+              {
+                item_id: chosenItem.item_id
+              }
+            ],
+            function(error) {
+              if (error) throw err;
+              console.log("Congrats! You have purchased " + answer.buy + " units!");
+              console.log("There are currently " + (chosenItem.stock_quantity - answer.buy) + " units remaining" );
+              connection.end();
+            }
+          );
+        }
+        else {
+          console.log("Sorry, there is not enough of this item in stock.");
+          connection.end();
+        }
+      });
+  });
+}
